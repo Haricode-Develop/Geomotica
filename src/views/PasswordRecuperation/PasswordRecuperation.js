737a1d311302fd3bb9d772b48967da5836c83655
Carlos Barrientos <cdbarrientos@url.edu.gt>
2023-11-29 13:58:10 -0600

    Add support ffrom the recovery

--- src/views/PasswordRecuperation/PasswordRecuperation.js
+++ src/views/PasswordRecuperation/PasswordRecuperation.js
@@ -1,46 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PasswordRecuperation.css';
 import { API_BASE_URL } from "../../utils/config";
 import axios from "axios";
-
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-        const response = await axios.post(`${API_BASE_URL}/auth/login`, {
-          email: email,
-          password: password,
-        });
-  
-        if (response.data) {
-          login();
-          console.log("USUARIO ===============");
-          console.log(response.data.user);
-          setUserData(response.data.user);
-          navigate("/dashboard");
-        } else {
-          setError(
-            response.data.message ||
-              "Error en el inicio de sesión. Por favor, intenta de nuevo."
-          );
-        }
-      } catch (err) {
-        setError(
-          err.response?.data?.message ||
-            "Error en el inicio de sesión. Por favor, intenta de nuevo."
-        );
-      }
-}
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 
 export const PasswordRecuperation = () => {
+  const navigate = useNavigate();
+  const [emailRecovery, setEmailRecovery] = useState("");
+  const [errorRecovery, setErrorRecovery] = useState("");
+  const { login, setUserData } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${API_BASE_URL}/auth/passwordRecuperation`, {
+        email: emailRecovery,
+      });
+
+      if (response.data) {
+        login();
+        console.log("USUARIO ===============");
+        console.log(response.data.user);
+        setUserData(response.data.user);
+        navigate("/dashboard");
+      } else {
+        setErrorRecovery(
+          response.data.message ||
+          "Error en el inicio de sesión. Por favor, intenta de nuevo."
+        );
+      }
+    } catch (err) {
+      setErrorRecovery(
+        err.response?.data?.message ||
+        "Error en el inicio de sesión. Por favor, intenta de nuevo."
+      );
+    }
+  }
+
+
+
   return (
     <div className='password-recuperation'>
-        <form className='password-recuperation__form' onSubmit={handleSubmit}>
-            <input type='email' placeholder='Correo' className='input' />
-            <button className='button'>Enviar</button>
-            <button className='button go-back-button'>Volver</button>
-            
-        </form>
+      <form className='password-recuperation__form' onSubmit={handleSubmit}>
+        <input type="email"
+              placeholder="Correo"
+              className="input"
+              value={emailRecovery}
+              onChange={(e) => setEmailRecovery(e.target.value)} />
+        <button className='button'>Enviar</button>
+        <button className='button go-back-button'>Volver</button>
+        {errorRecovery && <p className="error-message">{errorRecovery}</p>}
+      </form>
+      
     </div>
   )
 }
