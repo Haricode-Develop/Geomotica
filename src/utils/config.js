0dcfb9ba0fe7885a0b889eeb57074ebaaf3ed974
Carlos Barrientos <barryphotos7@gmail.com>
2023-12-09 23:31:03 -0600

    Add some feasutres

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
 //export const API_BASE_URL = 'https://geomotica.ingeoproyectos.com/';
 export const API_BASE_URL = 'https://geomotica.ingeoproyectos.com/';
+//export const API_BASE_URL = 'http://localhost:3001';
