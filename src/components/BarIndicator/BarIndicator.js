a0a59cc6eccc30b3db1d614c0efcc29e9a313982
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-02-04 17:22:50 -0600

    Corrección titulos mapeo
    Add, Adjunto corrección de titulos del label de los colores en el mapeo.

--- src/components/BarIndicator/BarIndicator.js
+++ src/components/BarIndicator/BarIndicator.js
@@ -3,7 +3,8 @@ import './BarIndicatorStyle.css';
 
 const BarIndicator = ({ filterType }) => {
     let title = "";
-
+    let indicadorIzquierda = "Bajo";
+    let indicadorDerecha = "Alto";
     switch (filterType) {
         case "speed":
             title = "Velocidad";
@@ -16,9 +17,13 @@ const BarIndicator = ({ filterType }) => {
             break;
         case "autoPilot":
             title = "Piloto Automatico";
+            indicadorIzquierda = "Enganchado";
+            indicadorDerecha = "Desenganchado";
             break;
         case "autoTracket":
-            title = "Auto Tracket"
+            title = "Auto Tracket";
+            indicadorIzquierda = "Automático";
+            indicadorDerecha = "Manual";
             break;
         default:
             title = "Indicador";
@@ -28,11 +33,11 @@ const BarIndicator = ({ filterType }) => {
         <div className="bar-indicator">
             <div className="bar-title">{title}</div>
             <div className="bar">
-                <span className="label low">Bajo</span>
+                <span className="label low">{indicadorIzquierda}</span>
                 <div className="bar-container">
                     <div className="gradient-bar" />
                 </div>
-                <span className="label high">Alto</span>
+                <span className="label high">{indicadorDerecha}</span>
             </div>
         </div>
     );
